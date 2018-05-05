9e87a5b03944dce93d114b9f05b8334d445235a0
RocketWong <wzh07@hotmail.com>
2018-05-05 20:15:01 +0800

    thymeleaf-demo finish

--- src/main/java/com/wisely/ch7_3/Ch73Application.java
+++ src/main/java/com/wisely/ch7_3/Ch73Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
